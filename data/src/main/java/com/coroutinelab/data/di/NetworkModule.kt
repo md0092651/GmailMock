d95f32fe6f0c7425ffff990ec9d8603ca77b8615
md <mithilesh1dhupia@gmail.com>
2024-09-05 18:38:50 +0100

    Step 6 - Add Core module

---
+++ data/src/main/java/com/coroutinelab/data/di/NetworkModule.kt
@@ -0,0 +1,32 @@
+package com.coroutinelab.data.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Provides
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+object NetworkModule {
+
+    val networkJson = Json { ignoreUnknownKeys = true }
+
+    @Provides
+    fun provideBaseUrl() = "https://66d8348537b1cadd8053cedf.mockapi.io/"
+
+    @Singleton
+    @Provides
+    fun providesHttpLoggingInterceptor() = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(okHttpClient: OkHttpClient, url: String): Retrofit = Retrofit.Builder()
+        .baseUrl(url)
+        .addConverterFactory(networkJson.asConverterFactory("application/json".toMediaType()))
+        .build()
+
+}
