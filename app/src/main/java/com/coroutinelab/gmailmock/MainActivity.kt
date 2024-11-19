8eb129637925d8c0e00217246a82322116a6bdc5
md <mithilesh1dhupia@gmail.com>
2024-11-19 11:33:25 +0000

    Step 17 Added test and clean up

--- app/src/main/java/com/coroutinelab/gmailmock/MainActivity.kt
+++ app/src/main/java/com/coroutinelab/gmailmock/MainActivity.kt
@@ -1,32 +1,33 @@
 package com.coroutinelab.gmailmock
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Videocam
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -61,6 +62,7 @@ import com.coroutinelab.presentation.emaillist.EmailListScreen
 import com.coroutinelab.presentation.emaillist.mvi.EmailListContract
 import com.coroutinelab.presentation.emaillist.mvi.EmailListViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
@@ -78,11 +80,24 @@ class MainActivity : ComponentActivity() {
                     ModalNavigationDrawer(
                         drawerState = drawerState,
                         drawerContent = {
-                            DrawerContent()
+                            DrawerContent(drawerState, scope)
                         },
                         scrimColor = MaterialTheme.colorScheme.surfaceContainer.copy(alpha = 0.4f),
                         content = {
                             Scaffold(
+                                floatingActionButton = {
+                                    ExtendedFloatingActionButton(
+                                        text = { Text("Compose") },
+                                        icon = { Icon(Icons.Filled.Edit, contentDescription = "") },
+                                        onClick = {
+                                            scope.launch {
+                                                drawerState.apply {
+                                                    if (isClosed) open() else close()
+                                                }
+                                            }
+                                        }
+                                    )
+                                },
                                 topBar = {
                                     when (topAppbarState) {
                                         TopAppbarState.HOME -> HomeAppBar(
@@ -113,13 +128,25 @@ class MainActivity : ComponentActivity() {
                                             modifier = Modifier.fillMaxWidth(),
                                             horizontalArrangement = Arrangement.SpaceEvenly
                                         ) {
-                                            IconButton(onClick = { }) {
+                                            IconButton(onClick = {
+                                                Toast.makeText(
+                                                    this@MainActivity,
+                                                    "Email Clicked",
+                                                    Toast.LENGTH_SHORT
+                                                ).show()
+                                            }) {
                                                 Icon(
                                                     imageVector = Icons.Filled.Email,
                                                     contentDescription = "Email"
                                                 )
                                             }
-                                            IconButton(onClick = { }) {
+                                            IconButton(onClick = {
+                                                Toast.makeText(
+                                                    this@MainActivity,
+                                                    "Video Call Clicked",
+                                                    Toast.LENGTH_SHORT
+                                                ).show()
+                                            }) {
                                                 Icon(
                                                     imageVector = Icons.Filled.Videocam,
                                                     contentDescription = "Video Call"
@@ -210,24 +237,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DrawerContent() {
+fun DrawerContent(drawerState: DrawerState, scope: CoroutineScope) {
     val drawerItemList: List<DrawerData> = getDrawerItemsList()
-    Column(
-        modifier = Modifier
-            .fillMaxHeight()
-            .fillMaxWidth(0.8f)
-            .background(MaterialTheme.colorScheme.surface)
-    ) {
-        LazyColumn(modifier = Modifier.padding(16.dp)) {
-            items(
-                drawerItemList
-            ) {
-                when (it) {
-                    is DrawerData.Category -> DrawerCategory(it.title)
-                    DrawerData.Divider -> DrawerDivider()
-                    is DrawerData.Item -> DrawerMenuItem(it.title, it.icon) { }
-                    is DrawerData.Title -> DrawerTitleItem(it.title)
+    ModalDrawerSheet {
+        drawerItemList.forEach {
+            when (it) {
+                is DrawerData.Category -> DrawerCategory(it.title)
+                DrawerData.Divider -> DrawerDivider()
+                is DrawerData.Item -> DrawerMenuItem(it.title, it.icon) {
+                    scope.launch {
+                        drawerState.close()
+                    }
                 }
+                is DrawerData.Title -> DrawerTitleItem(it.title)
             }
         }
     }
--- core-ui/src/main/java/com/coroutinelab/coreui/component/NavigationDrawerItems.kt
+++ core-ui/src/main/java/com/coroutinelab/coreui/component/NavigationDrawerItems.kt
@@ -1,18 +1,14 @@
 package com.coroutinelab.coreui.component
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
-import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
@@ -20,6 +16,7 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun DrawerTitleItem(title: String) {
     Text(
+        modifier = Modifier.padding(8.dp),
         text = title,
         color = MaterialTheme.colorScheme.onSurfaceVariant,
         style = MaterialTheme.typography.titleSmall
@@ -28,26 +25,25 @@ fun DrawerTitleItem(title: String) {
 
 @Composable
 fun DrawerMenuItem(title: String, icon: ImageVector, onCLick: () -> Unit) {
-    Row(
-        modifier = Modifier.padding(8.dp),
-        horizontalArrangement = Arrangement.Start,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Icon(
-            imageVector = icon,
-            contentDescription = title,
-            tint = MaterialTheme.colorScheme.onSecondaryContainer,
-            modifier = Modifier.size(24.dp)
-        )
-
-        VerticalDivider(modifier = Modifier.width(8.dp))
-
-        Text(
-            text = title,
-            color = MaterialTheme.colorScheme.onSecondaryContainer,
-            style = MaterialTheme.typography.titleSmall
-        )
-    }
+    NavigationDrawerItem(
+        label = {
+            Text(
+                text = title,
+                color = MaterialTheme.colorScheme.onSecondaryContainer,
+                style = MaterialTheme.typography.titleSmall
+            )
+        },
+        icon = {
+            Icon(
+                imageVector = icon,
+                contentDescription = title,
+                tint = MaterialTheme.colorScheme.onSecondaryContainer,
+                modifier = Modifier.size(24.dp)
+            )
+        },
+        onClick = { onCLick() },
+        selected = false
+    )
 }
 
 @Composable
