57109c0644f4ad7047adb93c8d6e2f268a071359
md <mithilesh1dhupia@gmail.com>
2024-11-21 13:32:33 +0000

    Step 17 Added test and clean up

--- presentation/src/androidTest/java/com/coroutinelab/presentation/emaillist/EmailListScreenTest.kt
+++ presentation/src/androidTest/java/com/coroutinelab/presentation/emaillist/EmailListScreenTest.kt
@@ -6,14 +6,9 @@ import androidx.compose.ui.test.isDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.coroutinelab.core.error.Failure
-import com.coroutinelab.domain.model.emaildetails.EmailDetailsModel
-import com.coroutinelab.domain.model.emaildetails.SenderInfoModel
 import com.coroutinelab.domain.model.emaillist.EmailListItemModel
-import com.coroutinelab.presentation.emaildetails.EmailDetailsScreen
-import com.coroutinelab.presentation.emaildetails.mvi.EmailDetailsContract
 import com.coroutinelab.presentation.emaillist.mvi.EmailListContract
 import org.junit.Rule
 import org.junit.Test
@@ -30,15 +25,12 @@ class EmailListScreenTest {
         val state = EmailListContract.EmailListState.Error(
             Failure.ServerError(500, "Server Error")
         )
-        val effect: EmailListContract.EmailListEffect? = null
         val dispatch: (EmailListContract.EmailListEvent) -> Unit = {}
 
         composeTestRule.setContent {
             EmailListScreen(
                 state = state,
-                effect = effect,
-                dispatch = dispatch,
-                onItemClick = {}
+                dispatch = dispatch
             )
         }
 
@@ -72,15 +64,12 @@ class EmailListScreenTest {
             )
         )
         val state = EmailListContract.EmailListState.Success(emailList)
-        val effect: EmailListContract.EmailListEffect? = null
         val dispatch: (EmailListContract.EmailListEvent) -> Unit = {}
 
         composeTestRule.setContent {
             EmailListScreen(
                 state = state,
-                effect = effect,
-                dispatch = dispatch,
-                onItemClick = {}
+                dispatch = dispatch
             )
         }
 
@@ -95,77 +84,16 @@ class EmailListScreenTest {
     @Test
     fun testLoadingState() {
         val state = EmailListContract.EmailListState.Loading
-        val effect: EmailListContract.EmailListEffect? = null
         val dispatch: (EmailListContract.EmailListEvent) -> Unit = {}
 
         composeTestRule.setContent {
             EmailListScreen(
                 state = state,
-                effect = effect,
-                dispatch = dispatch,
-                onItemClick = {}
+                dispatch = dispatch
             )
         }
 
         composeTestRule.onNodeWithContentDescription("Loading").assertExists()
     }
 
-    @Test
-    fun testEmailItemClickOpensEmailDetailsScreen() {
-        val emailItem = EmailListItemModel(
-            id = "id",
-            from = "test",
-            profileImage = "",
-            subject = "Test Subject",
-            snippet = "Snippet",
-            isStarred = false,
-            date = "date",
-            isPromotional = true,
-            isImportant = true
-        )
-
-        composeTestRule.setContent {
-            EmailListScreen(
-                state = EmailListContract.EmailListState.Success(listOf(emailItem)),
-                effect = null,
-                dispatch = {},
-                onItemClick = { email ->
-                    composeTestRule.setContent {
-                        EmailDetailsScreen(
-                            from = email.from,
-                            profileImage = email.profileImage,
-                            subject = email.subject,
-                            isPromotional = true,
-                            state = EmailDetailsContract.UIState(
-                                isLoading = false,
-                                isError = false,
-                                details = EmailDetailsModel(
-                                    from = SenderInfoModel("m", ""),
-                                    subject = email.subject,
-                                    date = email.date,
-                                    isStarred = email.isStarred,
-                                    isImportant = true,
-                                    isPromotional = true,
-                                    htmlBody = "htmlBody",
-                                    cc = emptyList(),
-                                    bcc = emptyList(),
-                                    id = email.id,
-                                    to = emptyList(),
-                                    labels = emptyList(),
-                                    fileInfo = emptyList()
-                                )
-                            )
-                        )
-                    }
-                }
-            )
-        }
-
-        // Simulate click on the email item
-        composeTestRule.onNodeWithText("Test Subject").performClick()
-
-        // Verify that the email details screen is displayed
-        composeTestRule.onNodeWithText("Test Subject").assertExists()
-        composeTestRule.onNodeWithText("Unsubscribe").assertExists()
-    }
 }
